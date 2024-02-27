adccc57720ddd776a7594abd0eb7b33eebca6c92
Jovan Tanasijevic <jocatanas47@gmail.com>
2024-02-27 07:40:30 +0100

    tidier and in callable form

--- script.py
+++ script.py
@@ -2,8 +2,9 @@ from deep_translator import GoogleTranslator
 import spacy
 import wiktionaryparser
 import csv
+import argparse
 from utils import load_dictionary
-from utils import add_lemmas
+from utils import load_sentences
 
 def get_wiktionary_entries(word):
     parser = wiktionaryparser.WiktionaryParser()
@@ -23,47 +24,46 @@ def word_to_definition(word):
                 string += "<br>"
     return string
 
-nlp = spacy.load("de_core_news_sm")
+def sentences_to_notes(sentences, lemmas):
+    nlp = spacy.load("de_core_news_sm")
+    translator = GoogleTranslator(source='de', target='en')
+    notes = []
+    for sentence in sentences:
+        note = []
 
-translator = GoogleTranslator(source='de', target='en')
+        modified_sentence = ""
+        definitions = ""
 
+        doc = nlp(sentence)
+        for token in doc:
+            lemma = token.lemma_
+            if lemma in lemmas:
+                modified_sentence += token.text
+            else:
+                modified_sentence += f"<b>{token.text}</b>"
+                definitions += word_to_definition(lemma)
+            modified_sentence += " "
 
-output = []
+        note.append(modified_sentence)
+        note.append(translator.translate(sentence))
+        note.append(definitions)
+        notes.append(note)
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_sentences")
+    parser.add_argument("input_dictionary")
+    parser.add_argument("output")
+    args = parser.parse_args()
+    sentences_txt = args.input_sentences
+    dictionary_txt = args.input_dictionary
+    output_csv = args.output
 
-def load_dictionary(filename):
-    dictionary = {}
-    with open(filename, 'r') as file:
-        for line in file:
-            key = line.strip()
-            dictionary[key] = True
-    return dictionary
+    sentences = load_sentences(sentences_txt)
+    lemmas = load_dictionary(dictionary_txt)
 
-lemmas = load_dictionary("test_lemas.txt")
-
-new_sentences = []
-for sentence in sentences:
-    new_output = []
-    new_sentence = ""
-    new_definitions = ""
-    doc = nlp(sentence)
-    for token in doc:
-        lemma = token.lemma_
-        if lemma in lemmas:
-            new_sentence += token.text
-        else:
-            new_sentence += f"<b>{token.text}</b>"
-            new_definitions += word_to_definition(lemma)
-        new_sentence += " "
-    new_sentences.append(new_sentence)
-    new_output.append(new_sentence)
-    new_output.append(translator.translate(sentence))
-    new_output.append(new_definitions)
-    output.append(new_output)
-
-file_name = 'data.csv'
-
-with open(file_name, 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerows(output)
-
+    notes = sentences_to_notes(sentences, lemmas)
+    
+    with open(output_csv, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(notes)
